770a925396c42e9e35b9718f4301f4dd9be9310b
Anurag Jaiswal <janurag582004@gmail.com>
2024-01-26 10:34:49 +0530

    .

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+
 let productImg = document.getElementById("productImg");
 let btn = document.getElementsByClassName("btn");
 
+
 btn[0].onclick = function(){
     productImg.src = "image1.png";
-
     for(bt of btn){
         bt.classList.remove("active");
     }
     this.classList.add("active");
 }
+
 btn[1].onclick = function(){
     productImg.src = "image2.png";
     for(bt of btn){
@@ -16,6 +18,7 @@ btn[1].onclick = function(){
     }
     this.classList.add("active");
 }
+
 btn[2].onclick = function(){
     productImg.src = "image3.png";
     for(bt of btn){
